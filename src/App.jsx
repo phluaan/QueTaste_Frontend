2d54a67dc9c851fcb348afea7ce8fc974ec0f399
MihnNathja <150262217+MihnNathja@users.noreply.github.com>
2025-08-23 10:20:48 +0700

    Create UserProfile flow vs Update ForgotPassword flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+import ProfileLayout from "./layouts/ProfileLayout";
 import Home from "./pages/Home";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import ForgotPassword from "./pages/auth/ForgotPassword";
+import VerifiedOTP from "./pages/auth/VerifiedOTP";
+import ProfilePage from "./pages/user/ProfilePage";
+
 
 function App() {
   return (
@@ -22,7 +26,16 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/verified-otp" element={<VerifiedOTP />} />
+
+      <Route path="/profile" element={<ProfileLayout />}>
+        <Route index element={<ProfilePage />} />
+        {/* Có thể thêm các route con khác */}
+        {/* <Route path="security" element={<SecurityPage />} /> */}
+        {/* <Route path="settings" element={<SettingsPage />} /> */}
+      </Route>
     </Routes>
+    
   );
 }
 
