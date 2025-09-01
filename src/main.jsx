6a6e63bedfc38c12917960d71c81cb1cc65e929c
Huynh-Minh-Man-04042004 <nsndman0404@gmail.com>
2025-09-01 15:57:36 +0700

    Update HomePage

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import { Provider } from "react-redux"; 
+import { Provider } from "react-redux";
 import store from "./redux/store";
 import App from "./App.jsx";
 
