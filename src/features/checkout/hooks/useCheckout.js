d4baa38272d483b51d29d8591ded559306c2ea8f
Huynh-Minh-Man-04042004 <nsndman0404@gmail.com>
2025-09-12 00:32:08 +0700

    feat(checkout): integrate Vietnam provinces API and update address handling

--- src/features/checkout/hooks/useCheckout.js
+++ src/features/checkout/hooks/useCheckout.js
@@ -1,12 +1,20 @@
 import { useDispatch, useSelector } from "react-redux";
 import { createOrder } from "../slices/checkoutSlice";
+import { showSuccess, showError } from "../../../utils/toastUtils";
 
 const useCheckout = () => {
     const dispatch = useDispatch();
     const { loading, error, order } = useSelector((state) => state.checkout);
 
-    const handleCheckout = (data) => {
-        dispatch(createOrder(data));
+    const handleCheckout = async (data) => {
+        try {
+        const result = await dispatch(createOrder(data)).unwrap();
+        showSuccess("Đặt hàng thành công! 🎉");
+        return result;
+        } catch (err) {
+        showError("Đặt hàng thất bại, vui lòng thử lại!");
+        throw err;
+        }
     };
 
     return { loading, error, order, handleCheckout };
