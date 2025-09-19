173e8e969280802e185d86c975d5b9d0b2778999
MihnNathja <150262217+MihnNathja@users.noreply.github.com>
2025-09-19 15:30:02 +0700

    Update routing for Order on UserMenu

--- src/features/user/pages/ProfilePage.jsx
+++ src/features/user/pages/ProfilePage.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
 import ProfileLayout from "../components/ProfileLayout";
 import ProfileForm from "../components/ProfileForm";
 import ChangePasswordForm from "../components/ChangePasswordForm";
 import SidebarButton from "../components/SideBarButton";
-import OrderForm from "../../order/components/OrderForm"
+import OrderForm from "../../order/components/OrderForm";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const ProfilePage = () => {
-  const [activeTab, setActiveTab] = useState("profile");
+  const [searchParams] = useSearchParams();
+  const tabFromUrl = searchParams.get("tab") || "profile";
+  const navigate = useNavigate();
 
   return (
     <ProfileLayout>
@@ -15,26 +17,26 @@ const ProfilePage = () => {
         <h2 className="text-xl font-bold mb-4">Cài đặt tài khoản</h2>
         <SidebarButton
           label="Thông tin cá nhân"
-          isActive={activeTab === "profile"}
-          onClick={() => setActiveTab("profile")}
+          isActive={tabFromUrl === "profile"}
+          onClick={() => navigate("/profile?tab=profile")}
         />
         <SidebarButton
           label="Đổi mật khẩu"
-          isActive={activeTab === "password"}
-          onClick={() => setActiveTab("password")}
+          isActive={tabFromUrl === "password"}
+          onClick={() => navigate("/profile?tab=password")}
         />
         <SidebarButton
           label="Đơn hàng của tôi"
-          isActive={activeTab === "orders"}
-          onClick={() => setActiveTab("orders")}
+          isActive={tabFromUrl === "orders"}
+          onClick={() => navigate("/profile?tab=orders")}
         />
       </div>
 
       {/* Nội dung chính */}
       <div className="w-4/5 p-6 min-h-[50vh] bg-white">
-        {activeTab === "profile" && <ProfileForm />}
-        {activeTab === "password" && <ChangePasswordForm />}
-        {activeTab === "orders" && <div><OrderForm /></div>}
+        {tabFromUrl === "profile" && <ProfileForm />}
+        {tabFromUrl === "password" && <ChangePasswordForm />}
+        {tabFromUrl === "orders" && <OrderForm />}
       </div>
     </ProfileLayout>
   );
